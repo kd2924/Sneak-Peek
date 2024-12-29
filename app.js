83ae2b96e31e5a556e6655cef6ec2f3b0ac42b15
kd2924 <147278225+kd2924@users.noreply.github.com>
2024-12-28 19:55:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function searchSneakers() {
   const sku = document.getElementById('skuInput').value.trim();
-  const baseUrl = 'https://sneak-peek-seven.vercel.app/api/sneakers';
+  const baseUrl = 'https://sneak-peek-seven.vercel.app/';
 
   if (!sku) {
     alert('Please enter a valid SKU!');
